e15f08f33e12abc2911635ce649a088fdfed587c
Chinh Bui <congchinhit94@gmail.com>
2017-03-06 11:22:32 +0700

    Merge branch 'master' of D:\Jobs\Android\android with conflicts.

--- app/src/main/java/com/its/its/view/WelcomeActivity.java
+++ app/src/main/java/com/its/its/view/WelcomeActivity.java
@@ -2,14 +2,30 @@ package com.its.its.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.its.its.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button btnPlay_Land, btnJoin_Land;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        initView();
+
+        addEvent();
+    }
+
+    private void addEvent() {
+
+    }
+
+    private void initView() {
+        btnPlay_Land = (Button) findViewById(R.id.btnPlay_Land);
+        btnJoin_Land = (Button) findViewById(R.id.btnJoin_Land);
     }
 }

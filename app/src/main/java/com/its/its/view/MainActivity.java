716f9664bf9539b1d442d166882fcf30388250db
Chinh Bui <congchinhit94@gmail.com>
2017-03-06 10:43:12 +0700

    welcome screen land

--- app/src/main/java/com/its/its/view/MainActivity.java
+++ app/src/main/java/com/its/its/view/MainActivity.java
@@ -1,12 +1,15 @@
 package com.its.its.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.its.its.R;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Chinh Bui on 3/6/2017.
+ */
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

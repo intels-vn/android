d9315527c4a5742b70de448964822a06e11fcc48
BiLac <luannm1410@gmail.com>
2017-03-10 09:38:35 +0700

    New Updates + set ID for layouts

---
+++ app/src/main/java/com/its/its/model/entity/UserRole.java
@@ -0,0 +1,17 @@
+package com.its.its.model.entity;
+
+/**
+ * Created by BiLac on 3/10/2017.
+ */
+
+public class UserRole {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

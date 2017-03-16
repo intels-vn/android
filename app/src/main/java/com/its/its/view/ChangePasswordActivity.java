9a451e71d1aa1f260b26ca582cbfeb5565ec814c
BiLac <luannm1410@gmail.com>
2017-03-16 17:03:18 +0700

    Updates: + Completed the validation of ChangePasswordActivity + Added new strings to ChangePasswordActivity(en, vi)

--- app/src/main/java/com/its/its/view/ChangePasswordActivity.java
+++ app/src/main/java/com/its/its/view/ChangePasswordActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.its.its.R;
-import com.its.its.model.tasks.ChangePasswordTask;
 import com.its.its.model.tasks.UpdateProfileTask;
 import com.its.its.presenter.security.EncryptDecrypt;
 
@@ -36,15 +36,52 @@ public class ChangePasswordActivity extends AppCompatActivity {
         btnApplyChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ChangePasswordTask(ChangePasswordActivity.this).execute(
-                        "http://192.168.100.14:8080/Demo/user/" +
-                                getIntent().getStringExtra("ID") + "/password?password=" +
-                                EncryptDecrypt.encrypt(edtNewPass.getText().toString(), getResources().getString(R.string.khoa)) +
-                                "&oldpassword=" +
-                                EncryptDecrypt.encrypt(edtOldPass.getText().toString(), getResources().getString(R.string.khoa)),
-                        getIntent().getStringExtra("TOKEN")
-                );
+                oldPass = edtOldPass.getText().toString();
+                newPass = edtNewPass.getText().toString();
+                retypePass_ChangePass = edtRetypePass_ChangePass.getText().toString();
+
+                if(validate()) {
+                    new UpdateProfileTask(ChangePasswordActivity.this).execute(
+                            "http://192.168.100.14:8080/Demo",
+                            EncryptDecrypt.encrypt(oldPass, getResources().getString(R.string.khoa)),
+                            EncryptDecrypt.encrypt(newPass, getResources().getString(R.string.khoa))
+                    );
+                }else{
+                    Toast.makeText(ChangePasswordActivity.this, getResources().getString(R.string.validate_empty_form), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        if(oldPass.isEmpty()){
+            edtOldPass.setError(getResources().getString(R.string.validate_old_password));
+            valid = false;
+        }
+
+        if(newPass.isEmpty()){
+            edtNewPass.setError(getResources().getString(R.string.validate_new_password));
+            valid = false;
+        }
+
+        if(retypePass_ChangePass.isEmpty()){
+            edtRetypePass_ChangePass.setError(getResources().getString(R.string.validate_retype_new_password));
+            valid = false;
+        }
+
+        if(newPass.equals(oldPass)){
+            edtNewPass.setError(getResources().getString(R.string.validate_matched_old_password));
+            valid = false;
+        }
+
+        if(retypePass_ChangePass.equals(newPass)){
+            edtRetypePass_ChangePass.setError(getResources().getString(R.string.validate_matched_retype_password));
+            valid = false;
+        }
+
+        return valid;
+    }
+
 }

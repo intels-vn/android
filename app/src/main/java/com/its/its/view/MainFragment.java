fee0af9c368a5bee65487e9364fc14e0fd3f6ee1
Chinh Bui <congchinhit94@gmail.com>
2017-03-30 17:08:07 +0700

    update Main

--- app/src/main/java/com/its/its/view/MainFragment.java
+++ app/src/main/java/com/its/its/view/MainFragment.java
@@ -1,5 +1,8 @@
 package com.its.its.view;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,17 +17,33 @@ import android.widget.VideoView;
 
 import com.its.its.R;
 
+import io.vov.vitamio.LibsChecker;
+
 public class MainFragment extends Fragment {
 
-    io.vov.vitamio.widget.VideoView videoView;
+    private io.vov.vitamio.widget.VideoView mVideoView;
     Button btnChannel1, btnChannel2, btnChannel3, btnChannel4, btnChannel5;
     Button btn100, btn200, btn300, btn400, btn500, btn600, btn700, btn800;
     Button btnBetNow;
+    private ProgressDialog prodlg;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//        if (!LibsChecker.checkVitamioLibs(getActivity()))
+//            return;
+//        getActivity().setContentView(R.layout.fragment_main);
+//        mVideoView.setVideoPath("rtmp://192.168.100.29:1935/oflaDemo/stream1");
+//        mVideoView.setMediaController(new io.vov.vitamio.widget.MediaController(getActivity()));
+//        mVideoView.requestFocus();
+//        mVideoView.start();
+//
+//        mVideoView.setOnPreparedListener(new io.vov.vitamio.MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(io.vov.vitamio.MediaPlayer mediaPlayer) {
+//                mediaPlayer.setPlaybackSpeed(1.0f);
+//            }
+//        });
     }
 
     @Override
@@ -32,7 +51,7 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        videoView = (io.vov.vitamio.widget.VideoView) view.findViewById(R.id.videoView);
+        mVideoView = (io.vov.vitamio.widget.VideoView) view.findViewById(R.id.videoView);
 
         btnChannel1 = (Button) view.findViewById(R.id.btnChannel1);
         btnChannel2 = (Button) view.findViewById(R.id.btnChannel2);
@@ -57,6 +76,61 @@ public class MainFragment extends Fragment {
     }
 
     private void addEvent() {
+        btnChannel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prodlg=new ProgressDialog(getActivity());
+                prodlg.setMessage("Waiting...");
+                prodlg.show();
+                runRtmp("rtmp://192.168.100.29:1935/oflaDemo/stream1", getActivity());
+            }
+        });
+        btnChannel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prodlg=new ProgressDialog(getActivity());
+                prodlg.setMessage("Waiting...");
+                prodlg.show();
+                runRtmp("rtmp://192.168.100.29:1935/oflaDemo/stream2", getActivity());
+            }
+        });
+        btnChannel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prodlg=new ProgressDialog(getActivity());
+                prodlg.setMessage("Waiting...");
+                prodlg.show();
+                runRtmp("rtmp://192.168.100.29:1935/oflaDemo/stream3", getActivity());
+            }
+        });
+        btnChannel4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prodlg=new ProgressDialog(getActivity());
+                prodlg.setMessage("Waiting...");
+                prodlg.show();
+                runRtmp("rtmp://192.168.100.29:1935/oflaDemo/stream5", getActivity());
+            }
+        });
+    }
 
+    public void runRtmp(String rtmpUrl, Activity activity){
+        if (!LibsChecker.checkVitamioLibs(activity))
+            return;
+
+        mVideoView.setVideoPath(rtmpUrl);
+        mVideoView.setMediaController(new io.vov.vitamio.widget.MediaController(activity));
+        mVideoView.setVideoQuality(io.vov.vitamio.MediaPlayer.VIDEOQUALITY_HIGH);
+        mVideoView.setBufferSize(2048);
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new io.vov.vitamio.MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(io.vov.vitamio.MediaPlayer mediaPlayer) {
+                prodlg.dismiss();
+                mediaPlayer.setPlaybackSpeed(1.0f);
+                mediaPlayer.setVolume(0f, 0f);
+                mVideoView.start();
+            }
+        });
     }
 }
